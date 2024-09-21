e007cb46d62e87d67c39c6bdcbefda87a0d39bcb
aji prasetyo <ajiprxty3@gmail.com>
2024-09-21 22:44:20 +0700

    parent menu

---
+++ lib/widget/parent_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ParentTitleMenu extends StatefulWidget {
+  const ParentTitleMenu({super.key});
+
+  @override
+  State<ParentTitleMenu> createState() => _ParentTitleMenuState();
+}
+
+class _ParentTitleMenuState extends State<ParentTitleMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
