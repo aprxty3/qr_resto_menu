8a1ff2b8a5943ed1ae9b6e0c0b4422e17b8ee54b
aji prasetyo <ajiprxty3@gmail.com>
2024-10-04 11:28:03 +0700

    handle on product_list

--- lib/widget/product_list.dart
+++ lib/widget/product_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../menu_state.dart';
 import '../model/menu_items.dart';
 import '../product_add.dart';
 
@@ -22,44 +23,62 @@ class _ProductListState extends State<ProductList> {
       create: (_) => ProductAdd(),
       child: Consumer<ProductAdd>(
         builder: (context, productCount, child) {
-          return Expanded(
-            child: ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.only(bottom: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: ListTile(
-                          title: Text('Nama Menu $index'),
-                          subtitle: const Text('Rp 10.000'),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          productCount.decrementTotal(index);
-                          widget.onProductCountChanged(
-                              productCount.getTotalCount());
-                        },
-                        icon: const Icon(Icons.remove),
-                      ),
-                      Text(productCount
-                          .getTotalForProduct(widget.items[index].id)
-                          .toString()),
-                      IconButton(
-                        onPressed: () {
-                          productCount.incrementTotal(index);
-                          widget.onProductCountChanged(
-                              productCount.getTotalCount());
-                        },
-                        icon: const Icon(Icons.add),
-                      ),
-                    ],
+          return Visibility(
+            visible: widget.items.isNotEmpty,
+            replacement: const Center(
+              child: Text('Produk Sedang Kosong'),
+            ),
+            child: Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.items.length,
+                      itemBuilder: (context, index) {
+                        final item = widget.items[index];
+                        return Card(
+                          margin: const EdgeInsets.only(bottom: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(item.name),
+                                  subtitle: Text(item.price.toString()),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  Provider.of<MenuState>(context, listen: false)
+                                      .updateProduct(item.id, -1);
+                                  productCount.decrementTotal(item.id);
+                                  widget.onProductCountChanged(
+                                      productCount.getTotalCount());
+                                },
+                                icon: const Icon(Icons.remove),
+                              ),
+                              Text(productCount
+                                  .getTotalForProduct(item.id)
+                                  .toString()),
+                              IconButton(
+                                onPressed: () {
+                                  Provider.of<MenuState>(context, listen: false)
+                                      .updateProduct(item.id, 1);
+                                  productCount.incrementTotal(item.id);
+                                  widget.onProductCountChanged(
+                                      productCount.getTotalCount());
+                                },
+                                icon: const Icon(Icons.add),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                );
-              },
+                  const SizedBox(height: 50),
+                ],
+              ),
             ),
           );
         },
